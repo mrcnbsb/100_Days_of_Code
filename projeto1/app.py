ed3a588578d263593a01be01113a0f618ca42787
mrcnbsb <marconebsb@gmail.com>
2025-11-13 09:13:15 -0300

    Projeto1

---
+++ projeto1/app.py
@@ -0,0 +1,4 @@
+print("Bem-vindo ao Gerador de Nome de Banda.")
+cidade = input("Qual cidade você nasceu?\n")
+pet = input("Qual o nome do seu animal de estimação?\n")
+print("O nome da sua banda poderia ser " + cidade + " " + pet)
