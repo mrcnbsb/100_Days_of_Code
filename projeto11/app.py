46839046b81deea56b2a78a21accaf6ce73fab4e
mrcnbsb <marconebsb@gmail.com>
2025-11-20 14:16:41 -0300

    Projeto11 v2

--- projeto11/app.py
+++ projeto11/app.py
@@ -2,120 +2,99 @@ import random
 import os
 from art import logo
 
+def dar_carta():
+    """Retorna uma carta aleatória do baralho"""
+    # Ás = 11 ou 1
+    # Valete, Dama e Rei = 10
+    # 2 = 2, 3 = 3, ..., 10 = 10
+    # lista com as cartas para sorteio com a random.choice
+    cartas = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    carta = random.choice(cartas)
+    return carta
+
+def calcular_pontuacao(cartas):
+    """Recebe uma lista de cartas e retorna a soma dos valores ou 0 se a soma igual a 21"""
+    # retorna 0 se Blackjack
+    if sum(cartas) == 21 and len(cartas) == 2:
+        return 0
+    # verica se há Ás(11) entre as cartas, se a soma ultrapassar 21 substitui 11 por 1
+    if 11 in cartas and sum(cartas) > 21:
+        cartas.remove(11)
+        cartas.append(1)
+    return sum(cartas)
 
 def limpar_tela():
+    """Limpa a tela do terminal"""
     os.system("cls")
 
-
-def exibir_maos(msg):
-    print(f"Sua mão final: {suas_cartas}, pontuação final: {sum(suas_cartas)}")
-    print(
-        f"Mão final da mesa: {pc_cartas}, pontuação final: {sum(pc_cartas)} ")
-    print(msg)
-
-
-def fazer_blackjack(cartas):
-    """Função que verifica se a soma das cartas é igual a 21"""
-    if sum(cartas) == 21:
-        return True
+def comparar_pontuacao(pont_usuario, pont_pc):
+    if pont_usuario == pont_pc:
+        return "Empate 🙃"
+    elif pont_pc == 0:
+        return "Você perdeu, a mesa fez um Blackjack. 😱"
+    elif pont_usuario == 0:
+        return "Venceu com um Blackjack. 😎"
+    elif pont_usuario > 21:
+        return "Você passou de 21. Você perdeu. 😭"
+    elif pont_pc > 21:
+        return "A mesa passou de 21. Você  venceu. 😬"
+    elif pont_usuario > pont_pc:
+        return "Você venceu. 😁"
     else:
-        return False
+        return "Você perdeu. 😫"
 
-# Ás = 11 ou 1
-# Valete, Dama e Rei = 10
-# 2 = 2, 3 = 3, ..., 10 = 10
-
-
-# lista com as cartas para sorteio com a random.choice
-cartas = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-# controle do fluxo do jogo
-quero_jogar = True
-
-limpar_tela()
-
-while quero_jogar:
+def jogar():
     print(logo)
+    # controle do fluxo do jogo, encerra o loop se True
+    e_fim_jogo = False
 
-    # verifica se o usuário quer jogar Blackjack
-    op1 = input("Você quer jogar Blackjack? Digite 's' ou 'n': ").lower()
+    usuario_cartas = []
+    pc_cartas = []
 
-    # caso o usuário queira jogar
-    if op1 == 's':
-        # limpa a tela com os.system('cls')
-        limpar_tela()
+    pontuacao_usuario = -1
+    pontuacao_pc = -1
 
-        # lista com duas cartas iniciais do usuário
-        suas_cartas = [random.choice(cartas), random.choice(cartas)]
+    # add duas cartas à lista do usuario e à lista da mesa
+    for _ in range(2):
+        usuario_cartas.append(dar_carta())
+        pc_cartas.append(dar_carta())
 
-        # lista com duas cartas iniciais da mesa (pc)
-        pc_cartas = [random.choice(cartas), random.choice(cartas)]
+    while not e_fim_jogo:
+        # add às pontuações do usuario e do pc o cálculo da soma das cartas
+        pontuacao_usuario = calcular_pontuacao(usuario_cartas)
+        pontuacao_pc = calcular_pontuacao(pc_cartas)
 
-        # verifica se a mesa fez blackjack, caso positivo vitória da mesa, neste ponto, empate é vitória da mesa
-        if fazer_blackjack(pc_cartas):
-            exibir_maos("A mesa fez um Blackjack. Você perdeu!!")
-            continue
-        # se a mesa não fez blackjack, mas o usuário fez blackjack, vitória do usuário
-        elif fazer_blackjack(suas_cartas):
-            exibir_maos("Você fez um Blackjack. Você ganhou!!")
-            continue
-        # se ninguém fez um blackjack na rodada inicial
+        # verifica se blackjack ou se usuario passou de 21
+        if pontuacao_usuario == 0 or pontuacao_pc == 0 or pontuacao_usuario > 21:
+            e_fim_jogo = True
         else:
-            while True:
-                # verifica se o usuário tem um 11 e se a pontuação é maior que 21, caso positivo troca o 11 por 1
-                if 11 in suas_cartas:
-                    if sum(suas_cartas) > 21:
-                        x = suas_cartas.index(11)
-                        suas_cartas[x] = 1
+            # exibi a rodada de cartas
+            print(f"\nSuas cartas: {usuario_cartas}, pontuação atual: {pontuacao_usuario}")
+            print(f"Primeira carta da mesa: {pc_cartas[0]}")
 
-               # verifica se a mesa tem um 11 e se a pontuação é maior que 21, caso positivo troca o 11 por 1
-                if 11 in pc_cartas:
-                    if sum(pc_cartas) > 21:
-                        x = pc_cartas.index(11)
-                        pc_cartas[x] = 1
+            # verifica se o usuario quer nova carta ou verificar o resultado do jogo (passar a mão)
+            op2 = input("Digite 's' para pegar outra carta, ou 'n' para passar: ").lower()
+            if op2 == 's':
+                usuario_cartas.append(dar_carta())
+            elif op2 == 'n':
+                e_fim_jogo = True
+            else:
+                print("Opção inválida!")
 
-                # exibi a rodada de cartas
-                print(
-                    f"Suas cartas: {suas_cartas}, pontuação atual: {sum(suas_cartas)}")
-                print(f"Primeira carta da mesa: {pc_cartas[0]}")
+    # add carta enquanto a pontuação do pc for inferior a 17
+    while pontuacao_pc < 17 and pontuacao_pc != 0:
+        pc_cartas.append(dar_carta())
+        pontuacao_pc = calcular_pontuacao(pc_cartas)
 
-                # verifica se o usuário quer nova carta ou verificar o resultado do jogo (passar a mão)
-                op2 = input(
-                    "Digite 's' para pegar outra carta, ou 'n' para passar: ").lower()
-                if op2 == 's':
-                    suas_cartas.append(random.choice(cartas))
-                    if fazer_blackjack(suas_cartas):
-                        exibir_maos("Você fez um Blackjack. Você ganhou!!")
-                        break
-                    elif sum(suas_cartas) > 21:
-                        exibir_maos("Você passou de 21. Você perdeu!!")
-                        break
-                elif op2 == 'n':
-                    while sum(pc_cartas) < 17:
-                        pc_cartas.append(random.choice(cartas))
-                        # verifica se a mesa fez blackjack, caso positivo vitória da mesa, neste ponto, empate é vitória da mesa
-                        if fazer_blackjack(pc_cartas):
-                            exibir_maos(
-                                "A mesa fez um Blackjack. Você perdeu!!")
-                            break
-                    # verifica se empate
-                    if sum(pc_cartas) > 21:
-                        exibir_maos("A mesa passou de 21. Você ganhou!!")
-                        break
-                    elif sum(suas_cartas) == sum(pc_cartas):
-                        exibir_maos("Empate!!")
-                        break
-                    # verifica se usuário ganhou
-                    elif sum(suas_cartas) > sum(pc_cartas):
-                        exibir_maos("Você ganhou!!")
-                        break
-                    # se a mesa(pc) ganhou
-                    else:
-                        exibir_maos("Você perdeu!!")
-                        break
+    # exibe resultado final do jogo
+    print(f"\nSua mão final: {usuario_cartas}, pontuação final: {pontuacao_usuario}")
+    print(f"Mão final da mesa: {pc_cartas}, pontuação final: {pontuacao_pc} ")
+    print(comparar_pontuacao(pontuacao_usuario, pontuacao_pc))
 
-    elif op1 == 'n':
-        print("Até a próxima!")
-        quero_jogar = False
-    else:
-        print("Opção inválida")
+while input("\nVocê quer jogar Blackjack? Digite 's' ou 'n': ").lower() == 's':
+    limpar_tela()
+    jogar()
+
+print("Até a próxima!")
+
+
