3991a9981ab8b882310f6bd61283b7107d4f799f
mrcnbsb <marconebsb@gmail.com>
2025-12-21 16:53:26 -0300

    EnglishFlashCards

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
 def generate_password():
@@ -42,22 +44,60 @@ def save_data():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {website:{
+        "email":email,
+        "password":password,
+    }}
 
-    if website == "" or email == "" or password == "":
+    if website == "" or password == "":
         # showinfo
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:  \nEmail: {email} "
-                                                      f"\nPassword: {password} \nIs it ok to save?") # True or False
-
-        if is_ok:
-            with open("data.txt", "a", encoding="utf-8") as file:
-                file.write(f"{website} | {email} | {password}\n")
-            website_entry.delete(0, tk.END)
-            password_entry.delete(0, tk.END)
-            website_entry.focus()
+        # is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:  \nEmail: {email} "
+        #                                               f"\nPassword: {password} \nIs it ok to save?") # True or False
+        #
+        # if is_ok:
+        #     with open("data.txt", "a", encoding="utf-8") as file:
+        #         file.write(f"{website} | {email} | {password}\n")
+        #     website_entry.delete(0, tk.END)
+        #     password_entry.delete(0, tk.END)
+        #     website_entry.focus()
+        # else:
+        #     website_entry.focus()
+        try:
+            with open("data.json", "r", encoding="utf-8") as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w", encoding="utf-8") as data_file:
+                # saving new data
+                json.dump(new_data, data_file, indent=4)
         else:
-            website_entry.focus()
+            # updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w", encoding="utf-8") as data_file:
+                # saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+                website_entry.delete(0, tk.END)
+                password_entry.delete(0, tk.END)
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", encoding="utf-8") as data_file:
+            # reading old data
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data.keys():
+            messagebox.showinfo(title=website, message=f"Email: {data[website]['email']} \n"
+                                                       f"Password: {data[website]['password']}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for the {website} exists.")
+    finally:
+        website_entry.delete(0, tk.END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -65,39 +105,55 @@ window = tk.Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = tk.Canvas(width=200, height=200)
+# Configuração das colunas
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=2)
+window.columnconfigure(2, weight=1)
+
+
+# Espaçamento padrão
+PADX = 10
+PADY = 6
+
+# Canvas (logo)
+canvas = tk.Canvas(window, width=200, height=200, highlightthickness=0)
 padlock_img = tk.PhotoImage(file="logo.png")
 canvas.create_image(100,100, image=padlock_img)
-canvas.grid(column=1, row=0)
+canvas.image = padlock_img  # evita garbage collector
+canvas.grid(column=0, row=0, columnspan=3, pady=(10,20))
 
 # labels
 website_label = tk.Label(text="Website:")
-website_label.grid(column=0, row=1)
+website_label.grid(column=0, row=1, sticky="e", padx=PADX, pady=PADY)
 
 email_label = tk.Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
+email_label.grid(column=0, row=2, sticky="e", padx=PADX, pady=PADY)
 
 password_label = tk.Label(text="Password:")
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=3, sticky="e", padx=PADX, pady=PADY) # sticky"e" -> Alinha os labels à direita, deixando tudo organizado.
 
 # entries
-website_entry = tk.Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = tk.Entry(window, width=30)
+website_entry.grid(column=1, row=1, padx=PADX, pady=PADY)
 website_entry.focus()
 
-email_entry = tk.Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
+email_entry = tk.Entry(window, width=40)
+email_entry.grid(column=1, row=2, columnspan=2, padx=PADX, pady=PADY)
 email_entry.insert(0, "marconebsb@gmail.com")
 
-password_entry = tk.Entry(width=21)
-password_entry.grid(column=1, row=3)
+password_entry = tk.Entry(window, width=21)
+password_entry.grid(column=1, row=3, padx=PADX, pady=PADY)
 
 # buttons
-generate_password_button = tk.Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(column=2, row=3)
+search_button = tk.Button(window, text="Search", width=13, command=find_password)
+search_button.grid(column=2, row=1, padx=PADX, pady=PADY)
 
-add_button = tk.Button(text="Add", width=36, command=save_data)
-add_button.grid(column=1, row=4, columnspan=2)
+generate_password_button = tk.Button(window, text="Generate Password", command=generate_password)
+generate_password_button.grid(column=2, row=3, padx=PADX, pady=PADY)
+
+add_button = tk.Button(window, text="Add", width=36, command=save_data)
+add_button.grid(column=1, row=4, columnspan=2, pady=(15, 0))
+
 
 
 
